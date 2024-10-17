c70a19327caf75d29d7d59fb1c081370e33933ac
Nakul Krishnakumar <nakulkrishnakumar86@gmail.com>
2024-10-18 01:09:33 +0530

    Third Commit

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,11 +5,11 @@ function ImageSlider() {
         <>  
             <div className="wrapper">
                 <div className="img-container">
-                    <img className="img" src="./src/assets/3x.png" alt="hello" />
-                    <img className="img" src="./src/assets/1a.png" alt="hello" />
-                    <img className="img" src="./src/assets/1b.png" alt="hello" />
-                    <img className="img" src="./src/assets/BACO.png" alt="hello" />
-                    <img className="img" src="./src/assets/v2.png" alt="hello" />
+                    <img className="img" src="https://github.com/nakul-krishnakumar/image-slider/blob/main/src/assets/3x.png?raw=true" alt="hello" />
+                    <img className="img" src="https://raw.githubusercontent.com/nakul-krishnakumar/image-slider/refs/heads/main/src/assets/1a.png" alt="hello" />
+                    <img className="img" src="https://github.com/nakul-krishnakumar/image-slider/blob/main/src/assets/1b.png?raw=true" alt="hello" />
+                    <img className="img" src="https://github.com/nakul-krishnakumar/image-slider/blob/main/src/assets/BACO.png?raw=true" alt="hello" />
+                    <img className="img" src="https://github.com/nakul-krishnakumar/image-slider/blob/main/src/assets/v2.png?raw=true" alt="hello" />
                 </div>
             </div>
         </>

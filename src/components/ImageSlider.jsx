191b7a65bb2966e8670eecf73b0ff453936b6c0e
Nakul Krishnakumar <nakulkrishnakumar86@gmail.com>
2024-10-18 00:54:20 +0530

    Second Commit

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -5,11 +5,11 @@ function ImageSlider() {
         <>  
             <div className="wrapper">
                 <div className="img-container">
-                    <img className="img" src="src\assets\3x.png" alt="hello" />
-                    <img className="img" src="src\assets\1a.png" alt="hello" />
-                    <img className="img" src="src\assets\1b.png" alt="hello" />
-                    <img className="img" src="src\assets\BACO.png" alt="hello" />
-                    <img className="img" src="src\assets\v2.png" alt="hello" />
+                    <img className="img" src="./src/assets/3x.png" alt="hello" />
+                    <img className="img" src="./src/assets/1a.png" alt="hello" />
+                    <img className="img" src="./src/assets/1b.png" alt="hello" />
+                    <img className="img" src="./src/assets/BACO.png" alt="hello" />
+                    <img className="img" src="./src/assets/v2.png" alt="hello" />
                 </div>
             </div>
         </>
